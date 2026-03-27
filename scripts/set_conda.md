206701148e02f6d52a13cacb82b1bd8ac1ccc2f0
erickfigue <e.figueroa.ildefonso@gmail.com>
2026-03-27 09:05:39 -0400

    Add new files for conda

---
+++ scripts/set_conda.md
@@ -0,0 +1,15 @@
+### Descarga e intala miniconda 
+
+Los pasos están en la web del programa
+
+https://www.anaconda.com/docs/getting-started/miniconda/install/linux-install#wget
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+bash ~/Miniconda3-latest-Linux-x86_64.sh
+
+Debe aceptarse los terminos y luego sleccionar la direcion donde instalar
+
+### Crea un environment usando conda
+
+conda create env --file intro_linux.yaml
---
+++ scripts/vcftools_filtering_snps.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+source /home/efigueroa/miniforge3/etc/profile.d/conda.sh
+conda activate popgen
+
+vcftools_filtering(){
+	local VCF=$1
+	local FN=$2
+    local OUT_DIR=$3
+    local alleles=$4
+    local mindp=$5
+    local maxmiss=$6
+    local minmaf=$7
+	local GQ=$8
+
+    OUT="${OUT_DIR}/${FN}_${alleles}all_dp${mindp}_miss${maxmiss}_maf${minmaf}_minGQ${GQ}"
+
+	vcftools --gzvcf $VCF \
+	--min-alleles $alleles --max-alleles $alleles \
+	--min-meanDP $mindp --minDP $mindp \
+	--max-missing $maxmiss --mac $minmaf \
+	--minGQ $GQ \
+	--out $OUT \
+	--recode --recode-INFO-all \
+	--stdout | bgzip -c > $OUT".vcf.gz"
+
+	tabix $OUT".vcf.gz"
+}
+
+OUT_DIR=/FastData/efigueroa/popgenomics/variants_gatk/mapping/diploid_vcfs/VNI_x1202/4_vcftools/1_snps_filtering
+VCF=/FastData/efigueroa/popgenomics/variants_gatk/mapping/diploid_vcfs/VNI_x1202/3_nocentromere/VNI_x1202_snps_HardFiltered_noCent.vcf.gz
+FN="VNI_x1202_nocent_diploid_snps"
+alleles=2
+mindp=5
+maxmiss=0.9
+minmaf=0
+GQ=30
+
+# Run the function 1
+#vcftools_filtering $VCF $FN $OUT_DIR $alleles $mindp $maxmiss $minmaf $GQ
+
+# Run the function 2
+alleles=2
+mindp2=5
+maxmiss2=0.9
+minmaf2=0.005
+GQ2=30
+#vcftools_filtering $VCF $FN $OUT_DIR $alleles $mindp2 $maxmiss2 $minmaf2 $GQ2
+
+# Run the function 2
+alleles=2
+mindp3=5
+maxmiss3=0.9
+minmaf3=4
+GQ3=30
+
+vcftools_filtering $VCF $FN $OUT_DIR $alleles $mindp3 $maxmiss3 $minmaf3 $GQ3
